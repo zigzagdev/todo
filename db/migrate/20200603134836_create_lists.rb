9f19786d8f65b3f8bf0a3430a2e83e9e0f705fa4
Masatoshi <masa@example.com>
2020-06-11 01:00:12 +0900

    リスト投稿のモデルの編集

--- db/migrate/20200603134836_create_lists.rb
+++ db/migrate/20200603134836_create_lists.rb
@@ -2,8 +2,7 @@ class CreateLists < ActiveRecord::Migration[6.0]
   def change
     create_table :lists do |t|
       t.string :name,null :false,comment: "投稿リスト"
-      t.text :content,null :false,comment: "投稿内容"
-
+      t.text :content,null :false,comment: "投稿内容について"
       t.timestamps
     end
   end
