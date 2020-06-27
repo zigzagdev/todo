fa2ff8672bdc8a66f5ce7a39ecbd1673b9b36007
Masatoshi <courtois.ibl@gmail.com>
2020-06-27 17:15:55 +0900

    テスト編集を行った

--- spec/factories/lists.rb
+++ spec/factories/lists.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-  FactoryBot.define do
-    factory :list do
-      sequence(:name) { |i|  "Test project #{i}"}
-      end
-    end
+FactoryBot.define do
+  factory :list do
+   sequence(:name) { |i|  "佐藤#{i}"}
+   sequence( :content) { |i|  "試しのテスト#{i}"}
+
+  end
+ end
---
+++ spec/models/list_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe List, type: :model do
+  describe "#title" do
+    context " " do
+      let(:list){list.new(title:" ")}
+
+     it "エラーを返す" do
+       list.valid?
+       expect(list.errors[:title]).to be_present
+       end
+    end
+  end
+    content "list" do
+      let(:list){list.new(title:"test title")}
+
+     it "エラーを返さない"do
+        list.valid?
+        expect(list.errorws[:title]).to be_brank
+     end
+    end
+end
