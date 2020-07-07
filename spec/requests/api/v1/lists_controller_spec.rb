4a6b33be40c66947c74d97afba44e027710884e2
Masatoshi <courtois.ibl@gmail.com>
2020-07-07 14:34:31 +0900

    rspecのrequest編集

--- spec/requests/api/v1/lists_controller_spec.rb
+++ spec/requests/api/v1/lists_controller_spec.rb
@@ -3,5 +3,24 @@
 
 
 RSpec.describe API::V1::Listcontroller, type: :request do
+ describe 'post/api/lists/:id' do
+   subject(:req) {post api_v1_lists_path, params: params, headers: headers }
+   context 'when authenticated' do
+     let(:headers) { {'Authorization'=> "Basic #{Base64.encode64('sample')}"} }
+     let(:params)  { {id: 'test1'} }
+     before(:each) { req }
+   it { expect(response.status). to rq 201}
+   it 'is expected to create list with given id' do
+       expect(List.find_by(id: 'test1')).not_to be nil
+     end
+   end
 
+   context 'when not authenticated' do
+     let(:headers) { { } }
+     let(:params)  { { } }
+     before(:each) { req}
+    it { expectct(response.status).to rq 401}
+   end
+
+ end
 end
